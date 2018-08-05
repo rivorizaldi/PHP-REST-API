3c439ff9eb262221d0a2dd8ee2afe95a1f5e8d81
rivorizaldi <rivo.rizaldy18@gmail.com>
2018-08-05 17:42:28 +0700

    adding read_single.php and read_single function

--- models/post.php
+++ models/post.php
@@ -43,5 +43,46 @@
 
         return $stmt;
     }
+
+    // Get Single Posts
+    public function read_single(){
+        //create query
+        $query = 'SELECT
+                    c.name as category_name,
+                    p.id,
+                    p.category_id,
+                    p.title,
+                    p.body,
+                    p.author,
+                    p.created_at
+                  FROM 
+                    '. $this->table .' p 
+                  LEFT JOIN
+                    categories c ON p.category_id = c.id 
+                  ORDER BY
+                    p.created_at DESC
+                  WHERE
+                  p.id = ?
+                  LIMIT 0,1';
+
+        //Prepare Statement
+        $stmt = $this->conn->prepare($query);
+
+        //Bind ID
+        $stmt->bindParam(1, $this->id);
+
+        //Execute query
+        $stmt->execute();
+
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        
+        //set property
+        $this->title = $row['title'];
+        $this->body = $row['body'];
+        $this->author = $row['title'];
+        $this->category_id = $row['category_id'];
+        $this->category_name = $row['category_name'];
+
+    }
   }
 ?>
