81e1caa84e6f9350635ab95ada92244b431ac97c
rivorizaldi <rivo.rizaldy18@gmail.com>
2018-08-05 17:07:23 +0700

    add read.php

---
+++ api/post/read.php
@@ -0,0 +1,54 @@
+<?php
+    //Headers
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+
+    include_once '../../config/Database.php';
+    include_once '../../models/Post.php';
+
+    // instantiate DB & Connect
+    $database = new Database();
+    $db = $database->connect();
+
+    // instantiate Blog Post Object
+    $post = new Post($db);
+
+    //blog post query
+    $result = $post->read();
+
+    //get row count
+    $num = $result->rowCount();
+
+    //Check if any posts
+    if ($num > 0) {
+        // Post Array
+        $posts_arr = array();
+        $posts_arr['data'] = array();
+
+        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+            extract($row);
+            $post_item = array(
+                'id' => $id,
+                'title' => $title,
+                'body' => html_entity_decode($body),
+                'author' => $author,
+                'category_id' => $category_id,
+                'category_name' => $category_name
+            );
+
+            // Push to "data"
+            array_push($posts_arr['data'],$post_item);
+        }
+
+        //Turn to JSON $ output
+        echo json_encode($posts_arr);
+    } else {
+        //No Post
+        echo json_encode(
+            array('message' => 'No Post Found')
+        );
+        
+    }
+    
+    
+?>
